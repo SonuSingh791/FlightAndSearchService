927cd2f8a29575cb1020e411940760fcde95e46c
Sonu Kumar <sonuflag159@gmail.com>
2023-12-28 00:39:57 +0530

    Installed basic packages to start the server

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const {PORT} = require('./config/serverConfig.js');
+
+
 
 const setUpAndStartServer = async function () {
     const app = express();
-
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
     app.listen(PORT, function(){
         console.log('Server started listening on port', PORT); 
     });
-
 };
 
 setUpAndStartServer(); 
